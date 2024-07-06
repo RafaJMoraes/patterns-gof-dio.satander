73d4dc5b6ebd8b0a4db4138267b08dfef14dc22b
RafaMoraes <rafaeljrmoraes@outlook.com>
2024-07-06 12:03:30 -0300

    :sparkles: feat: Via CEP Service com OpenFeign - Criando interface utilizando openFeign para conexao com via cep utilizando Springcloud

---
+++ src/main/java/dio/santander/desafios/pathernsgof/services/ViaCEPService.java
@@ -0,0 +1,14 @@
+package dio.santander.desafios.pathernsgof.services;
+
+
+import dio.santander.desafios.pathernsgof.domain.Address;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@FeignClient(name = "viacep",url = "https://viacep.com.br/ws/")
+public interface ViaCEPService {
+
+    @RequestMapping(path = "{cep/json/}")
+    Address foundCEP(@PathVariable("cep") String cep);
+}
